4313785509eec3c1c58a8d5b72b395a9a6de82d0
Nurlan Turganaliev <nurlan.turganaliev@gmail.com>
2023-04-02 21:17:09 +0600

    download books from all shown pages of a given category

--- tululu.py
+++ tululu.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import time
 
@@ -21,6 +22,23 @@ def check_for_redirect(response):
     if response.history:
         raise InvalidLinkException
     response.raise_for_status()
+
+
+def get_book_links_from_category(category_url):
+    link_list = []
+    response = requests.get(category_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    links = soup.find('div', {'id': 'content'}).find_all('a')
+    for link in links:
+        if (link['href'].startswith('/b')
+            and urljoin(category_url, link['href']) not in link_list):
+            link_list.append(urljoin(category_url, link['href']))
+    return link_list
+
+
+def get_book_id(link):
+    return urlparse(link).path.split('/')[1][1:]
 
 
 def download_txt(url, filename, folder='books/'):
@@ -50,9 +68,11 @@ def download_image(url, folder='images/'):
 def parse_book_page(content):
     all_comments = ''
     soup = BeautifulSoup(content, 'lxml')
-    title = soup.find(
+    title, author = soup.find(
         'div', {'id': 'content'}
-    ).find('h1').text.split("::")[0].strip()
+    ).find('h1').text.split("::")
+    title = title.strip()
+    author = author.strip()
     image_url = soup.find(
         'div', {'class': 'bookimage'}
     ).find('img')['src']
@@ -68,16 +88,20 @@ def parse_book_page(content):
     ).text.split(':')[1].strip().split(',')
     return {
         'title': title,
-        'image_url':image_url,
+        'author': author,
+        'image_url': image_url,
         'genre': genre,
         'comments': all_comments
     }
 
 
 def main():
-    url = 'https://tululu.org'
+    url = 'https://tululu.org/'
+    category_url = 'https://tululu.org/l55/'
     path = os.path.abspath('.')
-    folder = 'txt'
+    books_folder = 'books'
+    book_links = []
+    books_description = []
     parser = argparse.ArgumentParser(
         description = 'Enter first and last id'\
                       'to set range to download books.'
@@ -95,10 +119,17 @@ def main():
         type=int
     )
     args = parser.parse_args()
-    for book_id in range(args.start_id, args.end_id):
+    for page in range(args.start_id, args.end_id):
+        book_links += get_book_links_from_category(f'{category_url}{page}')
+
+    for index, link in enumerate(book_links):
         try:
-            book_url_reponse = requests.get(f'{url}/txt.php', {'id': book_id})
-            check_for_redirect(book_url_reponse)
+            book_id = get_book_id(link)
+            book_url_response = requests.get(
+                urljoin(url, 'txt.php'),
+                {'id': book_id}
+            )
+            check_for_redirect(book_url_response)
 
             page_response = requests.get(
                 f'{url}/b{book_id}/'
@@ -111,23 +142,27 @@ def main():
                 page_response.url,
                 parsed_content['image_url']
             )
-            filename = parsed_content['title']
-            genre = parsed_content['genre']
+            title = parsed_content['title']
+            author = parsed_content['author']
+            genres = parsed_content['genre']
             comments = parsed_content['comments']
             image_path = download_image(image_url)
             book_path = download_txt(
-                book_url_reponse.url,
-                f'{book_id}-{filename}',
-                folder
+                book_url_response.url,
+                f'{index}-я книга. {title}',
+                books_folder
             )
-            with open(
-                os.path.join(path, folder, 'comments.txt'),
-                'a'
-            ) as file:
-                file.write(comments)
-            print("Заголовок: ", filename)
+            books_description.append({
+                'title': title,
+                'author': author,
+                'img_src': image_path,
+                'book_path': book_path,
+                'comments': comments,
+                'gengres': genres
+            })
+            print("Заголовок: ", title)
             print(image_url)
-            print(genre)
+            print(genres)
             print()
         except InvalidLinkException:
             print("Exception occured: Link to download"\
@@ -139,6 +174,11 @@ def main():
             print(error)
             print()
             time.sleep(30)
+    with open(
+        os.path.join(path, books_folder, 'books_description.json'),
+        'wb',
+    ) as file:
+        file.write(json.dumps(books_description, ensure_ascii=False, indent=4).encode('utf8'))
 
 
 if __name__ == '__main__':
